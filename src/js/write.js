3dfa0224ff86aaaa7aed8b9d0847b2276508bb19
Frédéric van der Essen <fvdessen@gmail.com>
2013-06-22 00:02:42 +0200

    read zson from test page

--- src/js/write.js
+++ src/js/write.js
@@ -1,9 +1,9 @@
 $(function(){
     $('#to_zson').click(function(){
-        var zson = ZSON.encode(eval($('#json').val()));
+        var zson = ZSON.encode(eval($('#json-input').val()));
         
         window.webkitRequestFileSystem(window.TEMPORARY, zson.byteLength || 1024*1024, function(fs) {
-            fs.root.getFile($('#filename').val(), {create: true}, function(fileEntry) {
+            fs.root.getFile($('#filename-output').val(), {create: true}, function(fileEntry) {
                 fileEntry.createWriter(function(fileWriter) {
                     var arr = new Uint8Array(zson);
         
@@ -18,6 +18,14 @@ $(function(){
                 }, function() {});
             }, function() {});
         }, function() {});
-
+    });
+        
+    $('#filename-input').change(function(event){
+        var file = event.target.file || event.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function(event){
+            $('#json-output').val(JSON.stringify(ZSON.decode(event.target.result)));
+        };
+        reader.readAsArrayBuffer(file);
     });
 });
