e57ac970d133ff53cafc552222e3815d9a81cc29
Frédéric van der Essen <fvdessen@gmail.com>
2013-06-22 17:53:07 +0200

    show io errors on test page

--- src/js/readwrite.js
+++ src/js/readwrite.js
@@ -1,6 +1,14 @@
 $(function(){
     $('#to_zson').click(function(){
-        var zson = ZSON.encode(eval($('#json-input').val()));
+        try{
+            $('#error-input').css({'display':'none'});
+            var obj = eval('('+$('#json-input').val()+')');
+            var zson = ZSON.encode(obj);
+        }catch(e){
+            $('#error-input').text(e.toString());
+            $('#error-input').css({'display':'block'});
+            return;
+        }
         
         window.webkitRequestFileSystem(window.TEMPORARY, zson.byteLength || 1024*1024, function(fs) {
             fs.root.getFile($('#filename-output').val(), {create: true}, function(fileEntry) {
@@ -15,16 +23,22 @@ $(function(){
                     }, false);
         
                     fileWriter.write(blob);
-                }, function() {});
-            }, function() {});
-        }, function() {});
+                });
+            });
+        });
     });
         
     $('#filename-input').change(function(event){
+        $('#error-output').css({'display':'none'});
         var file = event.target.file || event.target.files[0];
         var reader = new FileReader();
         reader.onload = function(event){
-            $('#json-output').val(JSON.stringify(ZSON.decode(event.target.result)));
+            try{
+                $('#json-output').val(JSON.stringify(ZSON.decode(event.target.result)));
+            }catch(e){
+                $('#error-output').text(e.toString());
+                $('#error-output').css({'display':'block'});
+            }
         };
         reader.readAsArrayBuffer(file);
     });
